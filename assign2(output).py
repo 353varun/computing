e1b4b13d5154222091cb717ca9d74847b87dd057
353varun <97238778+353varun@users.noreply.github.com>
2022-01-26 13:13:58 +0530

    Add files via upload

---
+++ assign2(output).py
@@ -0,0 +1,40 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:\Users\Lenovo\Desktop\assignments\assign2(input).py ========
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive 
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+ 
+Hey, varun sharma Here! 
+My SID is 21104110 
+I am from Electrical Engineering and my CGPA is 9.900000
+ 
+8
+58
+50
+224 40
+14 0
+ 
+enter first no:
+9
+enter second no:
+6
+enter third no:
+13
+13.0  is the greatest no.
+ 
+enter input string
+this is my program
+no
+ 
+Enter length of first side
+3
+Enter length of second side
+8
+Enter length of third side
+6
+yes
